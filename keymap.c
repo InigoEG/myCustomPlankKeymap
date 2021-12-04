77f3125bb76e384f22b71d80960b0586b1b75755
Iñigo Echarri García <echarri.garcia@hotmail.com>
2021-12-04 14:22:10 +0100

    Capa game eliminada

--- keymap.c
+++ keymap.c
@@ -1,24 +1,7 @@
-/* Copyright 2015-2017 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 enum planck_layers {
   _QWERTY,
-  _GAME,
   _MOVE,
   _LOWER,
   _RAISE,
@@ -28,13 +11,11 @@ enum planck_layers {
 enum planck_keycodes {
   TD_LSHIFT_CAPS = 1,
   TD_RSHIFT_CAPS = 2,
-  QWERTY = 3,
-  GAME = 4
+  QWERTY = 3
 };
 
 #define LOW_BSPC LT(_LOWER, KC_BSPC)
 #define RAI_ENT LT(_RAISE, KC_ENT)
-#define GAME MO(_GAME)
 #define MOVE MO(_MOVE)
 
 qk_tap_dance_action_t tap_dance_actions[] = {
@@ -58,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * - Ctrl when held, Esc when tapped
     * - Lower when held, Bksp when tapped
     * - Raise when held, Enter when tapped
-    * - Shift + Shift = Caps On/Off
+    * - Double Shift = Caps On/Off
     */
     [_QWERTY] = LAYOUT_planck_grid(
         KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
@@ -101,24 +82,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
         _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-
-    /* Game
-     * ,-----------------------------------------------------------------------------------.
-     * |      |      |      |      |      |      |      |      |      |      |      |      |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | ESC  |      |      |      |      |      |      |      |      |      |      |      |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * |      |      |      |      |      |      |      |      |      |      |      |      |
-     * |------+------+------+------+------+------+------+------+------+------+------+------|
-     * | CTRL |      |      |      |      |             |      |      |      |      |      |
-     * `-----------------------------------------------------------------------------------'
-     */
-    [_GAME] = LAYOUT_planck_grid(
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_ESC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
     /* Move
