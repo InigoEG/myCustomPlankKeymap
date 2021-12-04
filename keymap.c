c89f8029d6364a36b5f06d105880608c3ec03947
Iñigo Echarri García <echarri.garcia@hotmail.com>
2021-12-04 14:24:03 +0100

    Tecla mute en tecla sin asignar y capa move replicada

--- keymap.c
+++ keymap.c
@@ -33,9 +33,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |-------+------+------+------+------+------+------+------+------+------+------+-------|
     * | Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |  ,<  |  .>  |  /?  | Shift |
     * |-------+------+------+------+------+------+------+------+------+------+------+-------|
-    * | Move  |      | OS   | Alt  |Lower*|    Space    |Raise*| Alt  | Vol- | Vol+ | Mute  |
+    * | Move  |      | OS   | Alt  |Lower*|    Space    |Raise*| Alt  | Vol- | Vol+ | Move  |
     * `-------------------------------------------------------------------------------------'
-    *
+    * 
     * - Ctrl when held, Esc when tapped
     * - Lower when held, Bksp when tapped
     * - Raise when held, Enter when tapped
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         LCTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         TD(TD_LSHIFT_CAPS),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, TD(TD_RSHIFT_CAPS),
-        MOVE,              XXXXXXX, KC_LGUI, KC_LALT, LOW_BSPC,KC_SPC,  KC_SPC,  RAI_ENT, KC_RALT, KC_VOLD, KC_VOLU, KC_MUTE
+        MOVE,              KC_MUTE, KC_LGUI, KC_LALT, LOW_BSPC,KC_SPC,  KC_SPC,  RAI_ENT, KC_RALT, KC_VOLD, KC_VOLU, MOVE
     ),
 
     /* Lower
